0bce4bbb6d765afd1fcb3e1cda9ab9a1fc8d3a7f
Jaysi <jaysi@jaysi.jaysi>
2024-02-15 17:49:40 +0530

    functions with parameter

--- Functions/function.py
+++ Functions/function.py
@@ -1,4 +1,11 @@
-def a():
-    print("aa")
 
-a()
+# Defining a function with def keyword and function name with parameters
+def area(length, breadth):
+
+    return (length*breadth) #returning the output
+
+# Storing the value in a variable
+result = area(10,5)
+
+#Printing the required value
+print("Area of rectangle is :", result)
